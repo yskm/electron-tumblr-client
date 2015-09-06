d809b48e25f11115e2960d435ce2ef0ba331bfb0
yskm <y.morita0216@gmail.com>
2015-09-06 19:27:41 +0900

    Add webview of tumblr.com

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1000, height: 600});
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
   mainWindow.on('closed', function() {
     mainWindow = null;
