10bb9105e52f9ffc707c32f927112e3220abb6c5
yskm <y.morita0216@gmail.com>
2015-09-20 01:30:29 +0900

    Refactor: toggling navigation

--- src/renderer/scripts/components/header-nav.js
+++ src/renderer/scripts/components/header-nav.js
@@ -7,41 +7,41 @@ export default class HeaderNav extends React.Component {
       isOpened: false
     };
   }
-  onClosedNavClick(e) {
-    e.preventDefault();
-    this.setState({isOpened: true});
-  }
-  onOpenedNavClick(e) {
-    e.preventDefault();
-    this.setState({isOpened: false});
-  }
-  render() {
-    if (this.state.isOpened) {
-      return (
-        <div className="ui vertical icon menu">
-          <a className="ui item" href="#" onClick={this.onOpenedNavClick.bind(this)}>
-            <i className="ellipsis vertical icon"></i>
-          </a>
-          <a className="ui item" href="#">
-            <i className="user icon"></i>
-          </a>
-          <a className="ui item" href="#">
-            <i className="icons">
-              <i className="user icon"></i>
-              <i className="inverted corner heart icon"></i>
-            </i>
-          </a>
-          <a className="ui item" href="#">
-            <i className="sign out icon"></i>
-          </a>
-        </div>
-      )
+
+  get subIcons() {
+    if (!this.state.isOpened) {
+      return;
     }
     return (
+      <div>
+        <a className="ui item" href="#">
+          <i className="user icon"></i>
+        </a>
+        <a className="ui item" href="#">
+          <i className="icons">
+            <i className="user icon"></i>
+            <i className="inverted corner heart icon"></i>
+          </i>
+        </a>
+        <a className="ui item" href="#">
+          <i className="sign out icon"></i>
+        </a>
+      </div>
+    );
+  }
+
+  toggleNav(e) {
+    e.preventDefault();
+    this.setState({isOpened: !this.state.isOpened});
+  }
+
+  render() {
+    return (
       <div className="ui vertical icon menu">
-        <a className="ui item" href="#" onClick={this.onClosedNavClick.bind(this)}>
+        <a className="ui item" href="#" onClick={this.toggleNav.bind(this)}>
           <i className="ellipsis vertical icon"></i>
         </a>
+        {this.subIcons}
       </div>
     );
   }
