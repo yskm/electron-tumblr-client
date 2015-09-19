dc27dd1db397e81bc906a7adcda32e2c48b775e4
yskm <y.morita0216@gmail.com>
2015-09-20 00:41:39 +0900

    Add feature: toggle navigation

--- src/renderer/scripts/components/header-nav.js
+++ src/renderer/scripts/components/header-nav.js
@@ -1,23 +1,46 @@
 import React from 'react';
 
 export default class HeaderNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpened: false
+    };
+  }
+  onClosedNavClick(e) {
+    e.preventDefault();
+    this.setState({isOpened: true});
+  }
+  onOpenedNavClick(e) {
+    e.preventDefault();
+    this.setState({isOpened: false});
+  }
   render() {
+    if (this.state.isOpened) {
+      return (
+        <div className="ui vertical icon menu">
+          <a className="ui item" href="#" onClick={this.onOpenedNavClick.bind(this)}>
+            <i className="ellipsis vertical icon"></i>
+          </a>
+          <a className="ui item" href="#">
+            <i className="user icon"></i>
+          </a>
+          <a className="ui item" href="#">
+            <i className="icons">
+              <i className="user icon"></i>
+              <i className="inverted corner heart icon"></i>
+            </i>
+          </a>
+          <a className="ui item" href="#">
+            <i className="sign out icon"></i>
+          </a>
+        </div>
+      )
+    }
     return (
       <div className="ui vertical icon menu">
-        <a className="ui item" href="#">
+        <a className="ui item" href="#" onClick={this.onClosedNavClick.bind(this)}>
           <i className="ellipsis vertical icon"></i>
-        </a>
-        <a className="ui item" href="#">
-          <i className="large user icon"></i>
-        </a>
-        <a className="ui item" href="#">
-          <i className="large icons">
-            <i className="user icon"></i>
-            <i className="inverted corner heart icon"></i>
-          </i>
-        </a>
-        <a className="ui item" href="#">
-          <i className="large sign out icon"></i>
         </a>
       </div>
     );
