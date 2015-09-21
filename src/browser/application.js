bc351296721ea841666794c0ea7b70080a64bba4
yskm <y.morita0216@gmail.com>
2015-09-21 13:49:57 +0900

    Refactor browser side

--- src/browser/application.js
+++ src/browser/application.js
@@ -1,26 +1,39 @@
 import app from 'app';
-import BrowserWindow from 'browser-window';
 import path from 'path';
-
-const mainWindowPath = path.resolve(__dirname, "..", "renderer", "index.html");
+import BrowserWindow from 'browser-window';
+import CrashReporter from 'crash-reporter';
 
 export default class Application {
   constructor() {
-    require('crash-reporter').start();
+    this.mainWindow = null;
+    this.mainWindowPath = path.resolve(__dirname, "..", "renderer", "index.html");
+    this.defaultWindowSize = {
+      width: 1000,
+      height: 800
+    }
 
-    let mainWindow = null;
+    this.initialize();
+  }
 
-    app.on('window-all-closed', function() {
+  initialize() {
+    CrashReporter.start();
+
+    app.on('window-all-closed', () => {
       if (process.platform != 'darwin')
         app.quit();
     });
 
-    app.on('ready', function() {
-      mainWindow = new BrowserWindow({width: 1000, height: 800});
-      mainWindow.loadUrl(`file://${mainWindowPath}`);
-      mainWindow.on('closed', function() {
-        mainWindow = null;
-      });
+    app.on('ready', () => {
+      this.createMainWindow();
+    });
+  }
+
+  createMainWindow() {
+    this.mainWindow = new BrowserWindow(this.defaultWindowSize);
+    this.mainWindow.loadUrl(`file://${this.mainWindowPath}`);
+
+    this.mainWindow.on('closed', function() {
+      this.mainWindow = null;
     });
   }
 }
